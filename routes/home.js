8b94c591188283e95d80a1afc1382592f822ffc0
Chung Sùng Khang <glennchung2000@gmail.com>
2022-05-28 18:05:14 +0700

    import and set secret express session

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
+
+router.use(session({ secret: 'bkttt' }));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    let context = {
-        title: 'Home | BKTPay',
-        layout:'layout'
-      }
+router.get('/', function (req, res, next) {
+  let context = {
+    title: 'Home | BKTPay',
+    layout: 'layout'
+  }
   res.render('home', context);
 });
 
