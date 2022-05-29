26503d72578787f265a6b1acfac809cc41cb4464
Chung Sùng Khang <glennchung2000@gmail.com>
2022-05-30 00:07:09 +0700

    add session checking

--- routes/home.js
+++ routes/home.js
@@ -3,10 +3,13 @@ var router = express.Router();
 var session = require('express-session');
 var credentials = require('../credentials');
 
-router.use(session({secret: credentials.session.key}));
+router.use(session({ secret: credentials.session.key }));
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  if (req.session.username === undefined) {
+    return res.redirect(303, '/');
+  }
   let context = {
     title: 'Home | BKTPay',
     layout: 'layout'
