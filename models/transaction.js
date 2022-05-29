9a73774cc6f9bbb30bc41ada173334dea3cdb399
Chung Sùng Khang <glennchung2000@gmail.com>
2022-05-29 17:44:41 +0700

    add transaction model

---
+++ models/transaction.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var transactionSchema = mongoose.Schema({
+    creator: String, // username 1: active
+    receiver: String, // username 2: passive
+    cardInfo: Object,
+    deposit: Number, // deposit not equal 0 --> withdraw equal 0 | if user 1 send coin to user 2 --> deposit not equal 0 and withdraw equal 0
+    withdraw: Number, // withdraw not equal 0 --> deposit equal 0
+    note: String, // note of creator
+    status: {
+        type: String,
+        default: 'THÀNH CÔNG',
+    }
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
