4f0e86d039a33c64badf7862452e0e0d0dc33ac6
Chung Sùng Khang <glennchung2000@gmail.com>
2022-05-31 21:43:30 +0700

    remove deposit and withdraw fields, add type and coin fields

--- models/transaction.js
+++ models/transaction.js
@@ -4,8 +4,8 @@ var transactionSchema = mongoose.Schema({
     creator: String, // username 1: active
     receiver: String, // username 2: passive
     cardInfo: Object,
-    deposit: Number, // deposit not equal 0 --> withdraw equal 0 | if user 1 send coin to user 2 --> deposit not equal 0 and withdraw equal 0
-    withdraw: Number, // withdraw not equal 0 --> deposit equal 0
+    type: String,
+    coin: Number,
     note: String, // note of creator
     created: {
         type: Date,
