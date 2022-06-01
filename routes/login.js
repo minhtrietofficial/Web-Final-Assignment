7da798e233f7a2599b36de20b8955655a5193b04
Chung Sùng Khang <glennchung2000@gmail.com>
2022-06-01 14:33:26 +0700

    completed login function version 2

--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,37 @@ var { body, validationResult } = require('express-validator');
 
 router.use(session({ secret: credentials.session.key }));
 
+function setLock(user) {
+    let failedLogin = 0;
+    let unusualLogin = 0;
+    let forbiddenTime = Date.now();
+    if (user.failedLogin == 3) {
+        failedLogin = 0;
+        unusualLogin = parseInt(user.unusualLogin) + 1;
+        forbiddenTime = Date.now() + 60;
+    } else {
+        failedLogin = parseInt(user.failedLogin) + 1;
+        unusualLogin = parseInt(user.unusualLogin);
+    }
+    User.updateOne(
+        { username: { $eq: user.username } },
+        {
+            $set: {
+                failedLogin: failedLogin,
+                unusualLogin: unusualLogin,
+                forbiddenTime: forbiddenTime,
+            }
+        }
+    )
+        .then(() => {
+            return true;
+        })
+        .catch(err => {
+            console.log(err);
+            return false;
+        })
+}
+
 router.get('/', (req, res) => {
     if (req.session.username !== undefined) {
         return res.redirect(303, '/home');
@@ -39,31 +70,80 @@ router.post('/',
         User.findOne({ username: username }, (err, row) => {
             if (err) console.log(err);
             if (row !== null) {
-                if (row.username == username) {
-                    bcrypt.compare(password, row.password, (err, result) => {
-                        if (err) console.log(err);
-                        if (result) {
-                            req.session.username = username;
-                            console.log(req.session.username)
-                            if (row.isFirstLogin) {
-                                return res.redirect(303, '/changepassword');
-                            } else {
-                                return res.redirect(303, '/home');
-                            }
-                        } else {
-                            let context = {
-                                title: 'Đăng nhập | BKTPay',
-                                layout: 'sublayout',
-                                errors: [
-                                    'Password is invalid'
-                                ],
-                            }
-                            res.status(401);
-                            return res.render('login', context);
+                if (row.unusualLogin < 2) {
+                    let time = Math.round((Date.now() - row.forbiddenTime) / 1000);
+                    if (time >= 60) {
+                        if (row.username == username) {
+                            bcrypt.compare(password, row.password, (err, result) => {
+                                if (err) console.log(err);
+                                if (result) {
+                                    req.session.username = username;
+                                    User.updateOne(
+                                        {username: {$eq: username}},
+                                        {
+                                            $set: {
+                                                failedLogin: 0,
+                                                unusualLogin: 0,
+                                            }
+                                        }
+                                    )
+                                    .then(() => {
+                                        if (row.isFirstLogin) {
+                                            return res.redirect(303, '/changepassword');
+                                        } else {
+                                            return res.redirect(303, '/home');
+                                        }
+                                    })
+                                    .catch(err => {
+                                        console.log(err);
+                                        let context = {
+                                            title: 'Đăng nhập | BKTPay',
+                                            layout: 'sublayout',
+                                            errors: [
+                                                'Server can not serve now'
+                                            ],
+                                        }
+                                        res.status(500);
+                                        return res.render('login', context);
+                                    })
+                                } else {
+                                    let context = {
+                                        title: 'Đăng nhập | BKTPay',
+                                        layout: 'sublayout',
+                                        errors: [
+                                            'Password is invalid'
+                                        ],
+                                    }
+                                    setLock(row);
+                                    res.status(401);
+                                    return res.render('login', context);
+                                }
+                            })
                         }
-                    })
+                    } else {
+                        let context = {
+                            title: 'Đăng nhập | BKTPay',
+                            layout: 'sublayout',
+                            errors: [
+                                `Account is locked for ${time} seconds`
+                            ],
+                        }
+                        console.log(context);
+                        res.status(401);
+                        return res.render('login', context);
+                    }
+                } else {
+                    let context = {
+                        title: 'Đăng nhập | BKTPay',
+                        layout: 'sublayout',
+                        errors: [
+                            `Your account is baned. Please contact admin to unlock`
+                        ],
+                    }
+                    console.log(context);
+                    res.status(401);
+                    return res.render('login', context);
                 }
-
             } else {
                 let context = {
                     title: 'Đăng nhập | BKTPay',
@@ -72,6 +152,7 @@ router.post('/',
                         'Username is invalid'
                     ],
                 }
+                setLock(row);
                 res.status(401);
                 return res.render('login', context);
             }
